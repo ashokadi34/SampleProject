bcb5f1c272d491b97a1e392ecead0713bfdbb6c6
ashokadi34 <ashokadi34@gmail.com>
2025-08-15 16:17:11 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/RemoveDupliCharsInString.java
@@ -0,0 +1,15 @@
+package simpleCodePractice;
+
+import java.util.stream.Collectors;
+
+public class RemoveDupliCharsInString {
+
+	public static void main(String[] args) {
+
+		String str="programming";			
+		String res=str.chars().distinct().mapToObj(c->(char)c).map(String::valueOf).collect(Collectors.joining());
+		System.out.println("removeDupliCharStringStreams: "+res);
+		
+	}
+
+}

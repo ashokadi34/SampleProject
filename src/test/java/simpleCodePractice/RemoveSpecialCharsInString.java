4cbb5158fba6ab3648a96805076e1f8e75cd877c
ashokadi34 <ashokadi34@gmail.com>
2025-08-17 10:23:21 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/RemoveSpecialCharsInString.java
@@ -0,0 +1,27 @@
+package simpleCodePractice;
+
+public class RemoveSpecialCharsInString {
+
+	public static void main(String[] args) {
+
+		String str="java@#%";		
+		String res="";	
+		int cou=0;
+		
+		for(int i=0;i<str.length();i++)
+		{
+			if(!Character.isDigit(str.charAt(i)) && !Character.isAlphabetic(str.charAt(i)) && !Character.isWhitespace(str.charAt(i)))
+			{
+				cou++;
+			}
+			else
+			{
+				
+				res=res+str.charAt(i);
+			}
+		}
+			System.out.println("removeSpecialCharsInString: "+res+" "+"[number of special chars in string: "+cou+"]");
+			
+	}
+
+}

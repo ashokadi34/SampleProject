bd79b5705f19debc921acecdbd2362694e169eb6
ashokadi34 <ashokadi34@gmail.com>
2025-08-27 21:55:23 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/FindLongestSubString.java
@@ -0,0 +1,33 @@
+package simpleCodePractice;
+
+import java.util.HashSet;
+
+public class FindLongestSubString {
+
+	public static void main(String[] args) {
+		
+		String str="pwwkew";
+		int start=0;		
+		int maxLength=0;
+		String res="";
+		HashSet<Character> set=new HashSet<>();
+		for(int end=0;end<str.length();end++)
+		{
+			while(set.contains(str.charAt(end)))
+			{
+				set.remove(str.charAt(start));
+				start++;
+			}
+				set.add(str.charAt(end));
+				if(end-start+1 > maxLength)
+				{
+					maxLength=end-start+1;
+					res=str.substring(start,end+1);
+				}
+		}		
+			//System.out.println("findLongestSubString: "+res+" "+res.length());
+			System.out.println("Longest substring: \""+res+"\" "+res.length());
+	
+	}
+
+}

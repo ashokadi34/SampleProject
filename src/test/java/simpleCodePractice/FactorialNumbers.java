359a015998c717084c150f1c4edd25b4dbd0bfaf
ashokadi34 <ashokadi34@gmail.com>
2025-08-30 09:41:03 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/FactorialNumbers.java
@@ -0,0 +1,16 @@
+package simpleCodePractice;
+
+public class FactorialNumbers {
+
+	public static void main(String[] args) {
+
+		int num=5;
+		int res=1;
+		for(int i=1; i<=num;i++) {
+			res=res*i;
+		}
+		System.out.println(res);
+	
+	}
+
+}

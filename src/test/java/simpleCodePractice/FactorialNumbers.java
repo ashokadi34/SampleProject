fa7d3df843df217ca8dc11c0f01b7ca007dd9e82
ashokadi34 <ashokadi34@gmail.com>
2025-08-30 09:43:15 +0530

    done small changes

--- src/test/java/simpleCodePractice/FactorialNumbers.java
+++ src/test/java/simpleCodePractice/FactorialNumbers.java
@@ -5,11 +5,12 @@ public class FactorialNumbers {
 	public static void main(String[] args) {
 
 		int num=5;
-		int res=1;
+		System.out.println("Given number: "+num);
+		int fact=1;
 		for(int i=1; i<=num;i++) {
-			res=res*i;
+			fact=fact*i;
 		}
-		System.out.println(res);
+		System.out.println("Factorial for given number: "+fact);
 	
 	}
 

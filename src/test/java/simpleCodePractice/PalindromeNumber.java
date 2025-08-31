bb2c54e6c01db90efbdf80ff36744a44c1e4bf19
ashokadi34 <ashokadi34@gmail.com>
2025-08-31 13:55:51 +0530

    Done small chages

--- src/test/java/simpleCodePractice/PalindromeNumber.java
+++ src/test/java/simpleCodePractice/PalindromeNumber.java
@@ -4,7 +4,8 @@ public class PalindromeNumber {
 
 	public static void main(String[] args) {
 
-		int num=42245;
+		int num=4224;
+		System.out.println("Given number: "+num);
 		int orgNum=num;
 		int rev=0;
 		for(;num!=0;num=num/10) {
@@ -13,10 +14,10 @@ public class PalindromeNumber {
 		}
 		
 		if(orgNum==rev) {
-			System.out.println("palindrome: "+rev);			
+			System.out.println("Number is palindrome: "+rev);			
 		}
 		else {
-			System.out.println("Not palindrome: "+rev);
+			System.out.println("Number is not palindrome: "+rev);
 		}			
 	
 	}

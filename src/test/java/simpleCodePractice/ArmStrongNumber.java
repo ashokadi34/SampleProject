f7ab242f7b31ded52702fbf28afee7e5732364a7
ashokadi34 <ashokadi34@gmail.com>
2025-09-02 08:52:42 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/ArmStrongNumber.java
@@ -0,0 +1,26 @@
+package simpleCodePractice;
+
+public class ArmStrongNumber {
+
+	public static void main(String[] args) {
+
+		int num=154;	//1*1*1 + 5*5*5 + 3*3*3=153
+		int orgNum=num;
+		int res=0;
+		int numLength=String.valueOf(num).length();
+		
+		while(num!=0) 
+		{
+			int r=num%10;
+			res=(int) (res+Math.pow(r, numLength));
+			num=num/10;			
+		}
+			if(res==orgNum) {
+				System.out.println(orgNum+" armStrongNumber");
+			} else {
+				System.out.println(orgNum+" not armStrongNumber");
+			}
+	
+	}
+
+}

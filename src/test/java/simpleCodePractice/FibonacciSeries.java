37a2f3a32a183b62d8591b264406ec01fa2a63ea
ashokadi34 <ashokadi34@gmail.com>
2025-09-03 10:22:24 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package simpleCodePractice;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int febCount=15;
+		int[] feb=new int[febCount];
+		feb[0]=0;
+		feb[1]=1;
+		for(int i=2;i<feb.length;i++) {
+			feb[i]=feb[i-1]+feb[i-2];
+		}
+		for(int i=0;i<feb.length;i++) {
+			System.out.print(feb[i]+" ");
+		}
+	
+	}
+
+}

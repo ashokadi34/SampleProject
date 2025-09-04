94e5a2b7e8c10e7c42e992eac4578d97351c903f
ashokadi34 <ashokadi34@gmail.com>
2025-09-04 07:41:06 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/PrimeNumbers.java
@@ -0,0 +1,24 @@
+package simpleCodePractice;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+
+		int n=18;
+		System.out.println("Given number: "+n);
+		boolean isPrime=true;
+		for(int i=2;i<=n/2;i++) {
+			if(n%i==0) {
+				isPrime=false;
+				break;
+			}			
+		}
+		if(isPrime && n>1) {
+			System.out.println(n+" is prime");
+		} else {
+			System.out.println(n+" is not prime");
+		}
+		
+	}
+
+}

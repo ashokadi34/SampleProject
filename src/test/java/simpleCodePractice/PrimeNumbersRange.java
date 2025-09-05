ccaff9eb4273b887fb56ecc1dc8fca1fec42e6f9
ashokadi34 <ashokadi34@gmail.com>
2025-09-05 11:04:43 +0530

    done small changes

--- src/test/java/simpleCodePractice/PrimeNumbersRange.java
+++ src/test/java/simpleCodePractice/PrimeNumbersRange.java
@@ -5,7 +5,8 @@ public class PrimeNumbersRange {
 	public static void main(String[] args) {
 		
 		int range =100;
-		System.out.println("prime numbers upto range: "+range);		
+		System.out.println("prime numbers upto range: "+range);	
+		int cou=0; 
 		for(int i=2;i<=range;i++) 
 		{	
 			boolean isPri=true;
@@ -18,8 +19,11 @@ public class PrimeNumbersRange {
 			}
 				if(isPri) {
 					System.out.print(i+" ");
+					cou++;	// Increment the counter if the number is prime			
 				}
+				
 		}
+			System.err.println("Total count of prime numbers: "+cou);
 		
 	}
 

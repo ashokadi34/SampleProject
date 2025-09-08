547cd23f8d201bf0b30e3c52ce36e3776a270799
ashokadi34 <ashokadi34@gmail.com>
2025-09-08 09:04:51 +0530

    done small changes

--- src/test/java/simpleCodePractice/CountOfDuplicateIntInArray.java
+++ src/test/java/simpleCodePractice/CountOfDuplicateIntInArray.java
@@ -5,7 +5,9 @@ public class CountOfDuplicateIntInArray {
 	public static void main(String[] args) {
 
 		int arr[] = {1,2,3,4,5,2,2};
+		System.out.println("Given Array: "+(java.util.Arrays.toString(arr)));
 		int in = 2;
+		System.out.println("Duplicate Int in Array(input): "+in);
 		int cou=0;
 		
 		for(int i : arr)
@@ -15,7 +17,7 @@ public class CountOfDuplicateIntInArray {
 				cou++;
 			}
 		}
-		System.out.println("duplicateIntCountArray: "+cou);
+		System.out.println("Count of Duplicate Int In Array: "+cou);
 	
 	}
 

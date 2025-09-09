965434f755b369e14c7be5025cfe4fc7255178c6
ashokadi34 <ashokadi34@gmail.com>
2025-09-09 08:37:16 +0530

    done small changes

--- src/test/java/simpleCodePractice/SumOfIntInArray.java
+++ src/test/java/simpleCodePractice/SumOfIntInArray.java
@@ -5,6 +5,7 @@ public class SumOfIntInArray {
 	public static void main(String[] args) {
 
 		int arr[] = {1,2,3,4,5};
+		System.out.println("Given Array: "+(java.util.Arrays.toString(arr)));
 		int sum=0;
 		
 		for(int i=0;i<arr.length;i++)
@@ -12,7 +13,7 @@ public class SumOfIntInArray {
 			sum=sum+arr[i];
 		}
 		
-		System.out.println("sumOfintInArray: "+sum);
+		System.out.println("Sum of Integers in Array: "+sum);
 	
 	}
 

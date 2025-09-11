919090aff4a83d0df961aaf9fcd7cf6d3c060d38
ashokadi34 <ashokadi34@gmail.com>
2025-09-11 07:41:59 +0530

    done small changes

--- src/test/java/simpleCodePractice/SumOfInteger.java
+++ src/test/java/simpleCodePractice/SumOfInteger.java
@@ -5,6 +5,7 @@ public class SumOfInteger {
 	public static void main(String[] args) {
 
 		int n =12345;
+		System.out.println("Given Integer: "+n);
 		int sum = 0;
 		
 		for(;n!=0;n=n/10)
@@ -13,7 +14,7 @@ public class SumOfInteger {
 			sum=sum+s;
 		}
 		
-		System.out.println("sumOfInt: "+sum);
+		System.out.println("Sum Of Int: "+sum);
 	
 	}
 

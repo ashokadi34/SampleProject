d2f477499af668c181cd4ee9edc08086b33c86eb
ashokadi34 <ashokadi34@gmail.com>
2025-09-13 08:39:09 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/BasicScannerCalculator.java
@@ -0,0 +1,42 @@
+package simpleCodePractice;
+
+import java.util.Scanner;
+
+public class BasicScannerCalculator {
+
+	public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();           
+            
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+        
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        double result;
+
+        switch(operator) {
+            case '+': result = num1 + num2;                
+                break;
+            case '-': result = num1 - num2;                
+                break;
+            case '*': result = num1 * num2;                
+                break;
+            case '/': result = num1 / num2;                
+                break;
+            default: System.out.println("Error: Invalid operator");
+               
+                return;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        
+    
+	}
+
+}

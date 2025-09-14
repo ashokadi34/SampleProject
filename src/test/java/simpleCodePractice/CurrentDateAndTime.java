86184592de7c03f065dd065a93b511bb1c177f40
ashokadi34 <ashokadi34@gmail.com>
2025-09-14 08:05:24 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/CurrentDateAndTime.java
@@ -0,0 +1,22 @@
+package simpleCodePractice;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class CurrentDateAndTime {
+
+	public static void main(String[] args) {
+
+		
+		 LocalDate d = LocalDate.now(); 
+		 
+		 System.out.println("Today date : "+d);
+		 
+		 LocalTime t = LocalTime.now();
+		 
+		 System.out.println("Time Now : "+t);
+
+	
+	}
+
+}

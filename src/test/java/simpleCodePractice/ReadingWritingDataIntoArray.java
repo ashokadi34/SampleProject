f4e0879aa41043058a8d2733da1da4b4df764865
ashokadi34 <ashokadi34@gmail.com>
2025-09-20 10:07:52 +0530

    new file added

---
+++ src/test/java/simpleCodePractice/ReadingWritingDataIntoArray.java
@@ -0,0 +1,28 @@
+package simpleCodePractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadingWritingDataIntoArray {
+
+	public static void main(String[] args) {
+
+		
+		int a[]=new int[5];
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		for(int i=0;i<a.length;i++) 
+		{
+			System.out.print("Enter a value for the position "+ i +" : ");
+			a[i]=sc.nextInt();
+		}
+		
+		System.out.println("Printing Array Elements.......");
+		System.out.println(Arrays.toString(a));
+		
+	
+	}
+
+}
